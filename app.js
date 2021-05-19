6e3002d68e24cf5ac2697822882adacd794f5816
Jalexander01 <60295314+Jalexander01@users.noreply.github.com>
2021-05-19 07:56:40 -0500

    adding links to posts

--- app.js
+++ app.js
@@ -86,6 +86,10 @@ app.post("/compose", function(req, res){
         titleVar: globalArrayPost.title,
         contentVar: globalArrayPost.content
       });
+      res.render('home', {
+        anykeyVar: _.lowerCase(m),
+
+      });
     }else{
 
     }
